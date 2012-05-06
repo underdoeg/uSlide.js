95e00c7c29304497802a896cfbfd22d6bc9a97f1
Philip Whitfield <public@underdoeg.com>
2012-05-06 16:44:46 +0200

    basic plugin structure

---
+++ js/uSlide.js
@@ -0,0 +1,13 @@
+(function( $ ) {
+  $.fn.uSlide = function(options) {
+    
+    //settings
+    var settings = $.extend( {
+        
+    }, options);
+
+    //execute plugin
+    return this.each(function() {        
+    });
+  };
+})( jQuery );
